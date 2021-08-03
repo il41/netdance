ad51f1bfdbeed4ec451e6a5f8ac8eec8964b7f2a
Tallon Hodge <tallonkh@gmail.com>
2021-08-02 22:57:43 -0400

    Invert Shape switches for more interesting filter interactions

--- tallon/js/video-filters.js
+++ tallon/js/video-filters.js
@@ -3,9 +3,20 @@ function lerp(a, b, x) {
 	return a + x * (b - a);
 }
 
+// switch between x and (1-x) based on i
+function toggleInvert(x, i) {
+	return i * 1 - x * (i * 2 - 1);
+}
+
 // linear interpolation between vec3s, using a float as the factor
 function lerp3(a3, b3, x) {
 	return [lerp(a3[0], b3[0], x), lerp(a3[1], b3[1], x), lerp(a3[2], b3[2], x), 1];
+}
+
+// linear interpolation between vec3s, using a float as the factor, invertable
+function lerp3i(a3, b3, x, invert) {
+	x = toggleInvert(x, invert);
+	return [lerp(a3[0], b3[0], xi), lerp(a3[1], b3[1], xi), lerp(a3[2], b3[2], xi), 1];
 }
 
 // linear interpolation between vec4s, using a float as the factor
@@ -16,6 +27,16 @@ function lerp4(a4, b4, x) {
 // linear interpolation between vec4s, using a vec3 as the factor
 function lerp3_3(a3, b3, x3) {
 	return [lerp(a3[0], b3[0], x3[0]), lerp(a3[1], b3[1], x3[1]), lerp(a3[2], b3[2], x3[2]), 1];
+}
+
+// linear interpolation between vec4s, using a vec3 as the factor, invertable
+function lerp3_3i(a3, b3, x3, invert) {
+	return [
+		lerp(a3[0], b3[0], toggleInvert(x3[0], invert)),
+		lerp(a3[1], b3[1], toggleInvert(x3[1], invert)),
+		lerp(a3[2], b3[2], toggleInvert(x3[2], invert)),
+		1,
+	];
 }
 
 function add3(a3, x) {
@@ -58,14 +79,17 @@ const vfTexture = new VideoFilterType(
 	(owner) => [
 		owner.shapeParameterCreator("Raw Video Frame", "Texture"),
 		owner.shapeParameterCreator("Chaotic Rectangles"),
+		{ name: "Invert Shape", type: "boolean", default: false },
 	],
 	() => {
-		return gpu.createKernel(function (frame, texture, mask) {
-			const x = this.thread.x;
-			const y = this.thread.y;
+		return gpu
+			.createKernel(function (frame, texture, mask, invertMask) {
+				const x = this.thread.x;
+				const y = this.thread.y;
 
-			return lerp3_3(frame[y][x], texture[y][x], mask[y][x]);
-		}).setFunctions([lerp, lerp3_3]);
+				return lerp3_3i(frame[y][x], texture[y][x], mask[y][x], invertMask);
+			})
+			.setFunctions([lerp, toggleInvert, lerp3_3i]);
 	}
 );
 
@@ -75,16 +99,20 @@ const vfTexture = new VideoFilterType(
  */
 const vfColor = new VideoFilterType(
 	"Color",
-	(owner) => [owner.shapeParameterCreator("Trails"), { name: "Color", type: "color", default: "#f008", alpha: true }],
+	(owner) => [
+		owner.shapeParameterCreator("Trails"),
+		{ name: "Invert Shape", type: "boolean", default: false },
+		{ name: "Color", type: "color", default: "#f008", alpha: true },
+	],
 	() => {
 		return gpu
-			.createKernel(function (frame, mask, color) {
+			.createKernel(function (frame, mask, invertMask, color) {
 				const x = this.thread.x;
 				const y = this.thread.y;
 
-				return lerp3_3(frame[y][x], color, mult3(mask[y][x], color[3]));
+				return lerp3_3i(frame[y][x], color, mult3(mask[y][x], color[3]), invertMask);
 			})
-			.setFunctions([lerp, lerp3_3, mult3]);
+			.setFunctions([lerp, toggleInvert, lerp3_3i, mult3]);
 	}
 );
 
@@ -96,6 +124,7 @@ const vfGradient = new VideoFilterType(
 	"Gradient",
 	(owner) => [
 		owner.shapeParameterCreator("Polygon"),
+		{ name: "Invert Shape", type: "boolean", default: false },
 		{ name: "Time", type: "number", source: "Time", hidden: true },
 		{ name: "Speed", type: "number", min: 0, max: 10, default: 0.5, step: 0.1 },
 		{ name: "Scale", type: "number", min: 1, max: 20, default: 1, step: 1 },
@@ -106,7 +135,7 @@ const vfGradient = new VideoFilterType(
 	],
 	() => {
 		return gpu
-			.createKernel(function (frame, mask, time, speed, scale, color1, color2, color3, color4) {
+			.createKernel(function (frame, mask, invertMask, time, speed, scale, color1, color2, color3, color4) {
 				// integer coords
 				const x = this.thread.x;
 				const y = this.thread.y;
@@ -130,9 +159,9 @@ const vfGradient = new VideoFilterType(
 					(Math.cos(rotfx * 3 + time * speed * 0.7) + 1) / 2
 				);
 
-				return lerp3_3(frame[y][x], gradientColor, mult3(mask[y][x], gradientColor[3]));
+				return lerp3_3i(frame[y][x], gradientColor, mult3(mask[y][x], gradientColor[3]), invertMask);
 			})
-			.setFunctions([lerp, lerp3_3, lerp4, iclamp, rotate, mult3]);
+			.setFunctions([lerp, toggleInvert, lerp3_3i, lerp4, iclamp, rotate, mult3]);
 	}
 );
 
@@ -144,28 +173,29 @@ const vfRGBLevels = new VideoFilterType(
 	"RGB Levels",
 	(owner) => [
 		owner.shapeParameterCreator("Everything"),
+		{ name: "Invert Shape", type: "boolean", default: false },
 		{ name: "Color", type: "color", default: "#fff", alpha: false },
-		{ name: "Invert", type: "boolean", default: false },
+		{ name: "Invert Colors", type: "boolean", default: false },
 	],
 	() => {
 		return gpu
-			.createKernel(function (frame, mask, color, invert) {
+			.createKernel(function (frame, mask, invertMask, color, invert) {
 				const x = this.thread.x;
 				const y = this.thread.y;
 				const pixel = frame[y][x];
-				const maskPixel = mask[y][x];
 
-				return lerp3_3(
+				return lerp3_3i(
 					pixel,
 					lerp3(
 						[pixel[0] * color[0], pixel[1] * color[1], pixel[2] * color[2]],
 						[1 - pixel[0] * color[0], 1 - pixel[1] * color[1], 1 - pixel[2] * color[2]],
 						invert
 					),
-					maskPixel
+					mask[y][x],
+					invertMask
 				);
 			})
-			.setFunctions([lerp, lerp3, lerp3_3]);
+			.setFunctions([lerp, toggleInvert, lerp3, lerp3_3i]);
 	}
 );
 
@@ -177,6 +207,7 @@ const vfWobble = new VideoFilterType(
 	"Wobble",
 	(owner) => [
 		owner.shapeParameterCreator("Everything"),
+		{ name: "Invert Shape", type: "boolean", default: false },
 		{ name: "Time", type: "number", source: "Time", hidden: true },
 		{ name: "Speed", type: "number", min: -10, max: 10, default: 1 },
 		{ name: "Frequency", type: "number", min: 0, max: 0.2, default: 0.05, step: 0.001 },
@@ -184,23 +215,24 @@ const vfWobble = new VideoFilterType(
 	],
 	() => {
 		return gpu
-			.createKernel(function (frame, mask, time, speed, frequency, intensity) {
+			.createKernel(function (frame, mask, invertMask, time, speed, frequency, intensity) {
 				const x = this.thread.x;
 				const y = this.thread.y;
 				const w = this.constants.width;
 				const h = this.constants.height;
 
 				const maskPixel = mask[y][x];
+				const maskVal = toggleInvert(maskPixel[0], invertMask);
 
-				const xFactor = Math.sin(time * speed + y * frequency) * w * intensity * maskPixel[0];
-				const yFactor = Math.cos(time * speed + x * frequency) * h * intensity * maskPixel[0];
+				const xFactor = Math.sin(time * speed + y * frequency) * w * intensity * maskVal;
+				const yFactor = Math.cos(time * speed + x * frequency) * h * intensity * maskVal;
 
 				const xWobble = Math.round(iclamp(x + xFactor, 0, w));
 				const yWobble = Math.round(iclamp(y + yFactor, 0, h));
 
 				return frame[yWobble][xWobble];
 			})
-			.setFunctions([iclamp]);
+			.setFunctions([toggleInvert, iclamp]);
 	}
 );
 
@@ -213,16 +245,17 @@ const vfMotionBlur = new VideoFilterType(
 	(owner) => [
 		owner.shapeParameterCreator("Last Output Frame", "Last Frame", true),
 		owner.shapeParameterCreator("Everything"),
+		{ name: "Invert Shape", type: "boolean", default: false },
 		{ name: "Intensity", type: "number", min: 0, max: 0.99, default: 0.8 },
 	],
 	() => {
 		return gpu
-			.createKernel(function (frame, lastFrame, mask, amount) {
+			.createKernel(function (frame, lastFrame, mask, invertMask, amount) {
 				const x = this.thread.x;
 				const y = this.thread.y;
-				return lerp3_3(frame[y][x], lastFrame[y][x], mult3(mask[y][x], amount));
+				return lerp3_3i(frame[y][x], lastFrame[y][x], mult3(mask[y][x], amount), invertMask);
 			})
-			.setFunctions([lerp, lerp3_3, mult3]);
+			.setFunctions([lerp, toggleInvert, lerp3_3i, mult3]);
 	}
 );
 
@@ -235,12 +268,13 @@ const vfZoomBlur = new VideoFilterType(
 	(owner) => [
 		owner.shapeParameterCreator("Last Output Frame", "Last Frame", true),
 		owner.shapeParameterCreator("Last Output Frame"),
+		{ name: "Invert Shape", type: "boolean", default: false },
 		{ name: "Intensity", type: "number", min: 0, max: 1, default: 0.75 },
 		{ name: "Scale", type: "number", min: 0.8, max: 1.2, default: 0.95 },
 	],
 	() => {
 		return gpu
-			.createKernel(function (frame, lastFrame, mask, amount, scale) {
+			.createKernel(function (frame, lastFrame, mask, invertMask, amount, scale) {
 				const x = this.thread.x;
 				const y = this.thread.y;
 				const w = this.constants.width;
@@ -251,8 +285,8 @@ const vfZoomBlur = new VideoFilterType(
 				const x2 = ((x - xOffset) * scale + xOffset) % w;
 				const y2 = ((y - yOffset) * scale + yOffset) % h;
 
-				return lerp3_3(frame[y][x], lastFrame[y2][x2], mult3(mask[y2][x2], amount));
+				return lerp3_3i(frame[y][x], lastFrame[y2][x2], mult3(mask[y2][x2], amount), invertMask);
 			})
-			.setFunctions([lerp, lerp3_3, mult3]);
+			.setFunctions([lerp, toggleInvert, lerp3_3i, mult3]);
 	}
 );
